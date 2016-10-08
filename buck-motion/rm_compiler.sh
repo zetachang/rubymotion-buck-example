f298ee1d5fcef67f0811b8169f4f5908c1903fd5
David Chang <zeta11235813@gmail.com>
2016-10-08 16:51:46 +0800

    Move hard coded SDK version to config file

--- buck-motion/rm_compiler.sh
+++ buck-motion/rm_compiler.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 /usr/bin/env \
-RM_DATADIR_PATH="/Library/RubyMotion/data/ios/9.3" \
+RM_DATADIR_PATH="/Library/RubyMotion/data/ios/$SDK_VERSION" \
 VM_PLATFORM="iPhoneSimulator" \
-VM_KERNEL_PATH="/Library/RubyMotion/data/ios/9.3/iPhoneSimulator/kernel-x86_64.bc" \
+VM_KERNEL_PATH="/Library/RubyMotion/data/ios/$SDK_VERSION/iPhoneSimulator/kernel-x86_64.bc" \
 VM_OPT_LEVEL="0" \
 /usr/bin/arch -arch x86_64 \
 "/Library/RubyMotion/bin/ruby" \
---uses-bs "/Library/RubyMotion/data/ios/9.3/BridgeSupport/RubyMotion.bridgesupport" \
---uses-bs "/Library/RubyMotion/data/ios/9.3/BridgeSupport/UIKit.bridgesupport" \
---uses-bs "/Library/RubyMotion/data/ios/9.3/BridgeSupport/Foundation.bridgesupport"  \
---uses-bs "/Library/RubyMotion/data/ios/9.3/BridgeSupport/CoreGraphics.bridgesupport" \
---uses-bs "/Library/RubyMotion/data/ios/9.3/BridgeSupport/CoreFoundation.bridgesupport" \
---uses-bs "/Library/RubyMotion/data/ios/9.3/BridgeSupport/CFNetwork.bridgesupport" \
---uses-bs "/Library/RubyMotion/data/ios/9.3/BridgeSupport/CoreAudio.bridgesupport" \
+--uses-bs "/Library/RubyMotion/data/ios/$SDK_VERSION/BridgeSupport/RubyMotion.bridgesupport" \
+--uses-bs "/Library/RubyMotion/data/ios/$SDK_VERSION/BridgeSupport/UIKit.bridgesupport" \
+--uses-bs "/Library/RubyMotion/data/ios/$SDK_VERSION/BridgeSupport/Foundation.bridgesupport"  \
+--uses-bs "/Library/RubyMotion/data/ios/$SDK_VERSION/BridgeSupport/CoreGraphics.bridgesupport" \
+--uses-bs "/Library/RubyMotion/data/ios/$SDK_VERSION/BridgeSupport/CoreFoundation.bridgesupport" \
+--uses-bs "/Library/RubyMotion/data/ios/$SDK_VERSION/BridgeSupport/CFNetwork.bridgesupport" \
+--uses-bs "/Library/RubyMotion/data/ios/$SDK_VERSION/BridgeSupport/CoreAudio.bridgesupport" \
 --emit-llvm-fast ""
